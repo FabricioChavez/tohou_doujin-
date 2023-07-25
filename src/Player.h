87ac9e80e93326cb519a6c3c0edf8c3760ecd466
Fabricio <fabriciochba@gmail.com>
2023-07-24 19:42:54 -0500

    character movement

---
+++ src/Player.h
@@ -0,0 +1,102 @@
+//
+// Created by USER on 7/24/2023.
+//
+
+#ifndef PROJECT_TOUHOU_PLAYER_H
+#define PROJECT_TOUHOU_PLAYER_H
+#include "iostream"
+#include "raylib.h"
+class  Player {
+
+private:
+    //Load player textures
+    Texture2D left{};
+    Texture2D right{};
+    Texture2D down{} ;
+    Texture2D up{};
+    //Player atributos
+    Rectangle sourceRect{}; //Place to draw player
+    float currentFrame =0; // set current frame
+    float frameTime = 0.0f; // set frame time
+    Vector2 initialPosition;
+    int spriteWidth_;
+    int spriteHeith_;
+    int divider =1 ;
+    float velx =2.0;
+    float vely=2.0;
+
+
+public:
+
+    explicit Player(const Vector2 &initialPosition) : initialPosition(initialPosition) {
+        left = LoadTexture("../assets/Marisa/LEFT.png");
+        right = LoadTexture("../assets/Marisa/RIGHT.png");
+        down = LoadTexture("../assets/Marisa/DOWN.png");
+        up = LoadTexture("../assets/Marisa/UP.png");
+
+    }
+
+  void FrameManager(float  new_frame )
+  {
+      spriteHeith_ = up.height;
+        if(IsKeyDown(KEY_W) or IsKeyDown(KEY_S))
+        {
+             divider =4;
+
+             spriteWidth_ = up.width/divider;
+        }
+        else if(IsKeyDown(KEY_A) or IsKeyDown(KEY_D))
+        {
+            divider=8;
+
+            spriteWidth_ = left.width/divider;
+        }else {
+            divider =4;
+
+            spriteWidth_ = up.width/divider;
+        }
+
+       frameTime+=new_frame;
+      if(frameTime > 0.5f)
+      {
+          frameTime =0.0f;
+          currentFrame++;
+            std::cout<<"DIVIDER :"<<divider<<std::endl;
+          if(currentFrame>divider)
+              currentFrame=0;
+
+          sourceRect.x = currentFrame*spriteWidth_;
+      }
+
+
+  }
+    void drawPlayer(float  new_frame ){
+        FrameManager(new_frame);
+
+        if(IsKeyDown(KEY_W)) {
+            sourceRect = {0, 0, static_cast<float>(spriteWidth_), static_cast<float>(spriteHeith_)};
+            initialPosition.y -= vely;
+            DrawTextureRec(up, sourceRect, initialPosition, WHITE);
+        } else if(IsKeyDown(KEY_A)) {
+            sourceRect = {0, 0, static_cast<float>(spriteWidth_), static_cast<float>(spriteHeith_)};
+            initialPosition.x -= velx;
+            DrawTextureRec(left, sourceRect, initialPosition, WHITE);
+        } else if(IsKeyDown(KEY_S)) {
+            sourceRect = {0, 0, static_cast<float>(spriteWidth_), static_cast<float>(spriteHeith_)};
+            initialPosition.y += vely;
+            DrawTextureRec(down, sourceRect, initialPosition, WHITE);
+        } else if(IsKeyDown(KEY_D)) {
+            sourceRect = {0, 0, static_cast<float>(spriteWidth_), static_cast<float>(spriteHeith_)};
+            initialPosition.x += velx;
+            DrawTextureRec(right, sourceRect, initialPosition, WHITE);
+        } else {
+            DrawTextureRec(down, sourceRect, initialPosition, WHITE);
+        }
+
+    }
+
+
+};
+
+
+#endif //PROJECT_TOUHOU_PLAYER_H
--- src/app.h
+++ src/app.h
@@ -6,6 +6,7 @@
 #define PROJECT_TOUHOU_APP_H
 #include "raylib.h"
 #include "global.h"
+#include "Player.h"
 using namespace std;
 Music music;
 bool paused = false;
@@ -13,23 +14,27 @@ struct app{
     static void runApp(){
         InitAudioDevice();
         InitWindow(screenWidth, screenHeight, "Blockchain Visualization");
+
+        Texture2D MarisaObject = LoadTexture("../assets/Marisa/RIGHT.png");
+        int spriteWidth = MarisaObject.width/8;
+        int spretHeigth = MarisaObject.height;
+        Rectangle sourceRect ={0,0,static_cast<float> (spriteWidth), static_cast<float > (spretHeigth)}; // Square to draw sprite in
+        float currentFrame =0; // set current frame
+        float frameTime = 0.0f; // set frame time
+        Vector2 position = { 720, 200 }; // windows postion intial
         SetTargetFPS(60);
-        bool showMenu = true;
-        bool shouldExit = false;
-//         PlayMusicStream(music);
-
-        // Redimensionar la imagen a un tamaño más pequeño
-        int temp = 0;
-        // Reproducir la música en bucle
-
-
-//    // Reproducir la música en bucle
-//        Image iconKAWAI = LoadImage("../textures/icon_windowscompress.png");
-//        SetWindowIcon(iconKAWAI);
+        Player Marisa(position);
         while (!WindowShouldClose()) {
+
+            frameTime += GetFrameTime();
+            //pra dibujar el frame
+
+
+
+
             BeginDrawing();
             ClearBackground(BLACK);
-
+            Marisa.drawPlayer(frameTime);
                 EndDrawing();
             }
         CloseWindow();
